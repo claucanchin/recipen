6372f6eabd25f6414cc076e7b2c59d9af0e8a10c
claucanchin <claucanchin@gmail.com>
2019-04-23 14:14:41 +0800

    heroku bug test

--- views/layout.jsx
+++ views/layout.jsx
@@ -12,7 +12,7 @@ class Layout extends React.Component {
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
                 <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Josefin+Slab" rel="stylesheet"></link>
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-                <link rel="stylesheet" href="/style.css"></link>
+                <link rel="stylesheet" href="/style.css" type="text/css"></link>
                 <title>{this.props.title}</title>
             </head>
 
